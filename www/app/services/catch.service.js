445cd6702793183b36a77f0402202be35da9c17f
Nevernown <ca.boel@student.avans.nl>
2016-05-03 21:21:12 +0200

    Concurrency fix!

--- www/app/services/catch.service.js
+++ www/app/services/catch.service.js
@@ -1,6 +1,6 @@
 angular.module('CatchService', []).factory('Catch', ['$state', '$timeout', function ($state, $timeout) {
   var curr = null;
-  var time = null;
+  var time = [];
   var target = null;
   var baseTimeMinutes = 0.1;
   var minTimePercent = 80;
@@ -8,41 +8,16 @@ angular.module('CatchService', []).factory('Catch', ['$state', '$timeout', funct
   var defaultRarity = 0.5;
 
   return {
-    zone: function (marker){
-      if(target != null){
-        if(time != null){ cancel(); }
-      } else if(curr == null) {
-        if(marker != null){
-          wait();
-        } else if (time != null) {
-          cancel();
-        }
-      }
-      curr = marker;
-    },
-    reset: function(){
-      target = null;
-      if(curr != null) { wait(); }
-    },
-    catch: function(){
-      if(trycatch()){
-        //do something.
-      }
-    }
+    zone: zone,
+    reset: reset,
+    catch: _catch
   };
-
-  function wait() {
-    time = $timeout(appear, rnTime());
-  }
-  function cancel() { $timeout.cancel(time); console.log(time); }
 
   function rnTime() {
     var ms = baseTimeMinutes*60*1000;
     var min = minTimePercent/100;
     var add = (maxTimePercent - minTimePercent)/100;
     var all = (min+(Math.random()*add))*ms;
-    //console.log(all);
-    //console.log(all/60/1000);
     return all;
   }
 
@@ -50,15 +25,42 @@ angular.module('CatchService', []).factory('Catch', ['$state', '$timeout', funct
     target = _.sample(curr.pokemon);
     $state.go('catch', {object: target});
   }
-  function trycatch(){
-   if(!target.rarity){
-     if(Math.random() <= defaultRarity){
-       console.log("success");
-       return true;
-     } else {
-       console.log("no luck");
-       return false;
-     }
-   }
+
+  function zone(marker){
+    if(target != null) {
+      $timeout.cancel(time);
+    } else {
+      time.push($timeout(appear, rnTime()));
+
+      if(curr == null && marker == null) {
+        stopTimers();
+      } else {
+        curr = marker;
+      }
+    }
+  }
+
+  function reset(){
+    stopTimers();
+    target = null;
+    zone(curr);
+  }
+
+  function stopTimers(){
+    var count = 0;
+    _.forEach(time, function(out){
+      count++;
+      console.log("Cancelling " + count + " timers");
+      $timeout.cancel(out);
+    });
+    time.length = 0;
+  }
+
+  function _catch(){
+    if(!target.rarity){
+      if(Math.random() <= defaultRarity){
+        console.log("Caught!");
+      }
+    }
   }
 }]);
