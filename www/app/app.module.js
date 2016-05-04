81c7d07ed94c775d226f000fa3884fcb592e4a89
erik--vink <erik.vink95@gmail.com>
2016-05-04 22:27:57 +0200

    Ionic ready test

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -27,8 +27,27 @@ var app = angular.module('app', [
 app.constant('baseApiUrl', "https://powerful-depths-54671.herokuapp.com/api/");
 app.constant('baseUrl', "https://powerful-depths-54671.herokuapp.com/");
 
-app.run(function ($rootScope, $location, Auth, $http, $window, $cookies, $state, Catch) {
+app.run(function ($ionicPlatform, $rootScope, $location, Auth, $http, $window, $cookies, $state, Catch) {
 
+  $ionicPlatform.ready(function() {
+    $state.transitionTo("login");
+    event.preventDefault();
+
+    //if(window.cordova && window.cordova.plugins.Keyboard) {
+    //    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    //    // for form inputs)
+    //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //
+    //    // Don't remove this line unless you know what you are doing. It stops the viewport
+    //    // from snapping when text inputs are focused. Ionic handles this internally for
+    //    // a much nicer keyboard experience.
+    //    cordova.plugins.Keyboard.disableScroll(true);
+    //}
+    //if(window.StatusBar) {
+    //    StatusBar.styleDefault();
+    //}
+    //navigator.splashscreen.hide();
+  });
 
   $rootScope.$on("$stateChangeStart",
     function (event, toState, toParams, fromState, fromParams) {
