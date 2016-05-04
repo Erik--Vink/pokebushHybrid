87ce5d3e336a5c0e40d31dafc952373e8dc6389b
erik--vink <erik.vink95@gmail.com>
2016-05-04 22:42:49 +0200

    Blank screen bug

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -27,27 +27,31 @@ var app = angular.module('app', [
 app.constant('baseApiUrl', "https://powerful-depths-54671.herokuapp.com/api/");
 app.constant('baseUrl', "https://powerful-depths-54671.herokuapp.com/");
 
-app.run(function ($ionicPlatform, $rootScope, $location, Auth, $http, $window, $cookies, $state, Catch) {
+app.run(function ($ionicPlatform, $rootScope, $location, Auth, $http, $window, $cookies, $state, Catch, $timeout) {
 
-  $ionicPlatform.ready(function() {
-    $state.transitionTo("login");
-    event.preventDefault();
+  //$ionicPlatform.ready(function() {
+  //  $state.transitionTo("login");
+  //  event.preventDefault();
+  //
+  //  //if(window.cordova && window.cordova.plugins.Keyboard) {
+  //  //    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+  //  //    // for form inputs)
+  //  //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  //  //
+  //  //    // Don't remove this line unless you know what you are doing. It stops the viewport
+  //  //    // from snapping when text inputs are focused. Ionic handles this internally for
+  //  //    // a much nicer keyboard experience.
+  //  //    cordova.plugins.Keyboard.disableScroll(true);
+  //  //}
+  //  //if(window.StatusBar) {
+  //  //    StatusBar.styleDefault();
+  //  //}
+  //  //navigator.splashscreen.hide();
+  //});
 
-    //if(window.cordova && window.cordova.plugins.Keyboard) {
-    //    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    //    // for form inputs)
-    //    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    //
-    //    // Don't remove this line unless you know what you are doing. It stops the viewport
-    //    // from snapping when text inputs are focused. Ionic handles this internally for
-    //    // a much nicer keyboard experience.
-    //    cordova.plugins.Keyboard.disableScroll(true);
-    //}
-    //if(window.StatusBar) {
-    //    StatusBar.styleDefault();
-    //}
-    //navigator.splashscreen.hide();
-  });
+  $timeout(function() {
+    $state.go('login');
+  }, 5000);
 
   $rootScope.$on("$stateChangeStart",
     function (event, toState, toParams, fromState, fromParams) {
